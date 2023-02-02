6beed29a4d18c8d7b00f6727e16d229dde9a6a78
Zachary Thompson <tho01699@ttu.edu>
2023-02-02 13:09:51 -0600

    changed app setting

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader(
-    //   'Access-Control-Allow-Headers',
-    //   'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    // );
-    // res.setHeader('Content-Type', 'application/json');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
