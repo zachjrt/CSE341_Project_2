9be777da63ea10f93a14200d25b5349bacd07463
Zachary Thompson <tho01699@ttu.edu>
2023-02-15 13:13:01 -0600

    PUSH

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.use('/', (req, res) => { res.sendFile(path.join(__dirname, '/static/index.html'))},  require('./swagger'));
+// router.use('/', (req, res) => { res.sendFile(path.join(__dirname, '/static/index.html'))},  require('./swagger'));
 
-// router.use('/', require('./swagger'));
+router.use('/', require('./swagger'));
 router.use('/requests', require('./requests'));
 router.use('/workers', require('./workers'));
 
