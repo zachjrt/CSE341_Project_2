a4296b2d72cfded5a0e9a47f9836575a55447c05
Zachary Thompson <tho01699@ttu.edu>
2023-02-22 09:42:59 -0600

    Index page working

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,15 @@ const path = require('path');
 //Not ready yet. Crashes
 // router.use('/', (req, res) => { res.sendFile(path.join(__dirname, '/static/index.html'))},  require('./swagger'));
 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../static/index.html'));
+  });
+
+router.get('/auth', (req, res) => {
+    res.redirect(
+      `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
+    );
+  });
 router.use('/', require('./swagger'));
 router.use('/requests', require('./requests'));
 router.use('/workers', require('./workers'));
